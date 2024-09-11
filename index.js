0d42fbda1cc4aa983c465a833e54f436299c9352
Habib <adebayohabib7@gmail.com>
2024-09-11 22:54:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require("express");
+const path = require('path');
 require("dotenv").config();
 const queryRoute = require("./routes/queryRoute");
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.use(express.json())
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.use("/api", queryRoute);
+
+app.use((req, res, next) => {
+  res.status(404).send('404: File Not Found');
+});
 
 app.listen(PORT, () => {
   console.log(`Application starts on PORT ${PORT}`);
