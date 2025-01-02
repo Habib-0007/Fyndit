608d3e527a276f3a541df108d705ef8e546d05d8
Habib-0007 <adebayohabib7@gmail.com>
2025-01-02 20:28:25 +0100

    fixed the bug issues in the app

--- controllers/queryGPT.js
+++ controllers/queryGPT.js
@@ -1,29 +1,55 @@
-const { gpt } = require("gpti");
+// const { gpt } = require("gpti");
+//
+// const askGPT = (req, res) => {
+//   res.set("Access-Control-Allow-Origin", "*");
+//   const { history, prompt } = req.body;
+//   try {
+//     gpt.v1(
+//       {
+//         messages: history,
+//         prompt: prompt,
+//         model: "GPT-4",
+//         markdown: false,
+//       },
+//       (err, data) => {
+//         if (err != null) {
+//           res.status(404).send(err);
+//         } else {
+//           res.status(201).send(data);
+//         }
+//       }
+//     );
+//   } catch (err) {
+//     res.status(500).send({ error: err.message });
+//   }
+// };
+//
+// module.exports = {
+//   askGPT,
+// };
+
+const { Hercai } = require("hercai");
 
 const askGPT = (req, res) => {
-  res.set("Access-Control-Allow-Origin", "*");
+	 res.set("Access-Control-Allow-Origin", "*");
   const { history, prompt } = req.body;
-  try {
-    gpt.v1(
-      {
-        messages: history,
-        prompt: prompt,
-        model: "GPT-4",
-        markdown: false,
-      },
-      (err, data) => {
-        if (err != null) {
-          res.status(404).send(err);
-        } else {
-          res.status(201).send(data);
-        }
-      }
-    );
-  } catch (err) {
-    res.status(500).send({ error: err.message });
-  }
+	try {
+		const herc = new Hercai();
+		herc
+			.question({
+				model: "v3",
+				content: "hi, how are you?",
+			})
+			.then(response => {
+				res.status(201).send(data);
+			});
+	} catch (err) {
+		res
+			.status(500)
+			.send({ error: err.message });
+	}
 };
 
 module.exports = {
-  askGPT,
+	askGPT,
 };
